3280406c191a9d8e39ba53c50e50f27a7516349c
Soumendra Kumar Sahoo <soumend1@in.ibm.com>
2020-08-09 14:54:19 +0530

    .txt file support added

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app = FastAPI()
 
 @app.post("/files/")
 async def create_files(file: UploadFile = File(...)):
+    if os.path.splitext(file.filename)[1] != '.txt':
+        return {"Error message": ".txt file format only permitted."}
     file2store = await file.read()
     csv_list = process_file(re.compile(r'\n(?=\d)').split(file2store.decode("utf-8")))
     op_filename = os.path.join(os.getcwd(), "report_file.csv")
